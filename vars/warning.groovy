fa7345e0786db81a4d7f37931c048090291a62a7
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-18 10:52:16 +0700

    Update warning.groovy

--- vars/warning.groovy
+++ vars/warning.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
   hello(name: "Kanha Phol", dayOfWeek:"Monday")
-  loadlinux(name:"hello_world.sh")
+  loadlinux(name:"build_and_run.sh")
   sh " ./hello_world.sh ${config.name} ${config.dayOfWeek}."
 }
